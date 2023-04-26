e30681ed029eca1b8dca73138041ba9aa6e8fb93
BrookeBrandenburger <125424826+BrookeBrandenburger@users.noreply.github.com>
2023-04-26 11:42:06 -0400

    added the test for queue

--- TestMovieQueue.java
+++ TestMovieQueue.java
@@ -9,6 +9,12 @@ public class TestMovieQueue {
         Movie movie2 = new Movie("The Dark Knight", 2008, 15639, 94, true);
         Movie movie3 = new Movie("Jurassic Park", 1993, 14281, 91, false);
         Movie movie4 = new Movie("The Godfather", 1972, 17893, 98, true);
+        Movie movie5 = new Movie("Inception", 2010, 12356, 87, false);
+        Movie movie6 = new Movie("Avatar", 2009, 19274, 82, true);
+        Movie movie7 = new Movie("The Shawshank Redemption", 1994, 13645, 91, true);
+        Movie movie8 = new Movie("Pulp Fiction", 1994, 11937, 94, false);
+        Movie movie9 = new Movie("Forrest Gump", 1994, 14582, 72, true);
+        Movie movie10 = new Movie("The Matrix", 1999, 16471, 87, true);
         
         a.enqueue(movie1);
         a.enqueue(movie2);
